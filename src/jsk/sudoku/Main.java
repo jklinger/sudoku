94744ff59aa5aed401d20f6c0432a658c47649c5
Justin <justin.s.klinger@gmail.com>
2011-11-07 22:46:47 -0600

    Removing third-party look-and-feel dependencies

--- src/jsk/sudoku/Main.java
+++ src/jsk/sudoku/Main.java
@@ -5,8 +5,6 @@ import static javax.swing.UIManager.getSystemLookAndFeelClassName;
 import static javax.swing.UIManager.setLookAndFeel;
 import static javax.swing.WindowConstants.DISPOSE_ON_CLOSE;
 
-import java.util.Properties;
-
 import javax.swing.UnsupportedLookAndFeelException;
 
 import jsk.sudoku.model.Board;
@@ -15,9 +13,6 @@ import jsk.sudoku.ui.SudokuSolver;
 import org.apache.log4j.ConsoleAppender;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PatternLayout;
-
-import com.jtattoo.plaf.aero.AeroLookAndFeel;
-import com.jtattoo.plaf.smart.SmartLookAndFeel;
 
 public class Main implements Runnable {
 	
@@ -38,40 +33,6 @@ public class Main implements Runnable {
 		setLookAndFeel(getSystemLookAndFeelClassName());
 	}
 
-	/*
-	private static void setupLookAndFeel() throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
-		Properties props = new Properties();
-
-		props.put("logoString", " _ ");
-		//props.put("licenseKey", "INSERT YOUR LICENSE KEY HERE");
-
-		props.put("disabledForegroundColor", "255 0 0");
-//		props.put("menuSelectionBackgroundColor", "180 240 197");
-
-		props.put("gridColor", "218 23 23");
-//		props.put("controlColorLight", "218 254 230");
-//		props.put("controlColorDark", "180 240 197");
-
-		props.put("buttonColor", "218 230 254");
-		props.put("buttonColorLight", "255 255 255");
-		props.put("buttonColorDark", "244 242 232");
-
-//		props.put("rolloverColor", "218 254 230");
-//		props.put("rolloverColorLight", "218 254 230");
-//		props.put("rolloverColorDark", "180 240 197");
-
-//		props.put("windowTitleForegroundColor", "0 0 0");
-//		props.put("windowTitleBackgroundColor", "180 240 197");
-//		props.put("windowTitleColorLight", "218 254 230");
-//		props.put("windowTitleColorDark", "180 240 197");
-//		props.put("windowBorderColor", "218 254 230");
-
-		// set your theme
-		AeroLookAndFeel.setCurrentTheme(props);
-		// select the Look and Feel
-		setLookAndFeel("com.jtattoo.plaf.aero.AeroLookAndFeel");
-	}
-	*/
 	public void run() {
 		SudokuSolver window = new SudokuSolver(new Board(BoardType.STANDARD));
 		window.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
